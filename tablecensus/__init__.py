c93c159235a8f1f8e9aea0241bbb34cd8a435cc1
Mike V <mike@datadrivendetroit.org>
2025-05-28 08:08:32 -0400

    moved assemble logic to its own module

--- tablecensus/__init__.py
+++ tablecensus/__init__.py
@@ -3,14 +3,9 @@ import shutil
 from importlib.resources import files, as_file
 import datetime
 import click
-import pandas as pd
 
-from .variables import create_renamer
-from .geography import build_api_geo_parts
-from .request_prep import build_calls
-from .request_manager import populate_data
+from .assemble import assemble_from
 from .table_style import apply_d3_style
-
 
 TODAY = datetime.date.today().strftime("%Y%m%d")
 
@@ -47,48 +42,8 @@ def start(destination):
 def assemble(dictionary_path, output_path):
     # TODO -- this is great for a cli, but I NEED to be callable from code as well
     print(f"Assembling data from dictionary {dictionary_path} and saving to {output_path}")
-
-    variables = pd.read_excel(dictionary_path, sheet_name="Variables")
-    geographies = pd.read_excel(
-        dictionary_path, sheet_name="Geographies", dtype="string"
-    )
-    releases = (
-        pd.read_excel(dictionary_path, sheet_name="Years")
-        .itertuples(index=False, name=None)
-    )
-    geo_parts = build_api_geo_parts(geographies)
-
-    rename = create_renamer(variables)
-    variable_codes = list(rename.keys())
     
-    calls = build_calls(geo_parts, variable_codes, releases)
-    responses = populate_data(calls)
-    
-    result = []
-    for response in responses:
-        (year, release), data = response
-        columns, *row = data
-        
-        frame = (
-            pd.DataFrame(row, columns=columns)
-            .rename(columns=rename)
-            .astype({var: pd.Int64Dtype() for var in rename.values()})
-            .assign(Year=year, Release=release)
-        )
-        result.append(frame)
-
-    namespace = pd.concat(result).reset_index(drop=True)
-
-    result = [namespace["GEO_ID"], namespace["NAME"], namespace["Year"], namespace["Release"]]
-    for _, variable in variables.iterrows():
-        result.append(
-            namespace.eval(variable["calculation"]).rename(variable["name"])
-        )
-
-    final = (
-        pd.concat(result, axis=1)
-        .rename(columns={"GEO_ID": "geoid", "NAME": "Geography Name"})
-    )
+    final = assemble_from(dictionary_path)
 
     apply_d3_style(final).to_excel(output_path, index=False)
 
---
+++ tablecensus/assemble.py
@@ -0,0 +1,50 @@
+import pandas as pd
+
+from .variables import create_renamer
+from .geography import build_api_geo_parts
+from .request_prep import build_calls
+from .request_manager import populate_data
+
+
+def assemble_from(dictionary_path):
+    variables = pd.read_excel(dictionary_path, sheet_name="Variables")
+    geographies = pd.read_excel(
+        dictionary_path, sheet_name="Geographies", dtype="string"
+    )
+    releases = (
+        pd.read_excel(dictionary_path, sheet_name="Years")
+        .itertuples(index=False, name=None)
+    )
+    geo_parts = build_api_geo_parts(geographies)
+
+    rename = create_renamer(variables)
+    variable_codes = list(rename.keys())
+    
+    calls = build_calls(geo_parts, variable_codes, releases)
+    responses = populate_data(calls)
+    
+    result = []
+    for response in responses:
+        (year, release), data = response
+        columns, *row = data
+        
+        frame = (
+            pd.DataFrame(row, columns=columns)
+            .rename(columns=rename)
+            .astype({var: pd.Int64Dtype() for var in rename.values()})
+            .assign(Year=year, Release=release)
+        )
+        result.append(frame)
+
+    namespace = pd.concat(result).reset_index(drop=True)
+
+    result = [namespace["GEO_ID"], namespace["NAME"], namespace["Year"], namespace["Release"]]
+    for _, variable in variables.iterrows():
+        result.append(
+            namespace.eval(variable["calculation"]).rename(variable["name"])
+        )
+
+    final = (
+        pd.concat(result, axis=1)
+        .rename(columns={"GEO_ID": "geoid", "NAME": "Geography Name"})
+    )
